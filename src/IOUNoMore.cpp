465e9c2200dc55c64cd561a5f93e640f50950ede
administrator <administrator@laptop.(none)>
2012-05-17 20:30:55 +0200

    box_muller added

--- src/IOUNoMore.cpp
+++ src/IOUNoMore.cpp
@@ -5,11 +5,73 @@
 // Copyright   : This code is intended as a proof-of-concept for a website that uses a network of IOU's and tries to cancel out as much IOU's as possible.
 // Description : Hello World in C++, Ansi-style
 //============================================================================
-
+#include <cstdlib> // for rand() and srand()
+#include <ctime> // for time()
+#include <math.h>
 #include <iostream>
+
+
 using namespace std;
+
+/***************************************************************************************************************
+boxmuller.c Implements the Polar form of the Box-Muller
+Transformation
+
+(c) Copyright 1994, Everett F. Carter Jr.
+Permission is granted by the author to use
+this software for any application provided this
+copyright notice is preserved.
+
+*/
+
+/* ranf() is uniform in 0..1 */
+float ranf(){
+
+int nLow = 0;
+int nHigh = RAND_MAX;
+int data = (rand() % (nHigh - nLow + 1)) + nLow;
+
+return (float)data/nHigh;
+}
+
+
+float box_muller(float m, float s) /* normal random variate generator */
+{ /* mean m, standard deviation s */
+float x1, x2, w, y1;
+static float y2;
+static int use_last = 0;
+
+if (use_last) /* use value from previous call */
+{
+y1 = y2;
+use_last = 0;
+}
+else
+{
+do {
+x1 = 2.0 * ranf() - 1.0;
+x2 = 2.0 * ranf() - 1.0;
+w = x1 * x1 + x2 * x2;
+} while ( w >= 1.0 );
+
+w = sqrt( (-2.0 * log( w ) ) / w );
+y1 = x1 * w;
+y2 = x2 * w;
+use_last = 1;
+}
+
+return( m + y1 * s );
+}
+
+
+
+
 
 int main() {
 	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	for(int i = 0; i < 10; i++){
+		cout << box_muller(5.0, 1.0) << endl;
+	}
+
 	return 0;
 }
