fd85b048f85c259b626c7ad66ac3adf69ff733cf
administrator <administrator@laptop.(none)>
2012-05-17 20:43:43 +0200

    added gaussian distribution

--- src/IOUNoMore.cpp
+++ src/IOUNoMore.cpp
@@ -9,9 +9,89 @@
 #include <ctime> // for time()
 #include <math.h>
 #include <iostream>
+#include <sstream>
+#include <fstream>
+#include <vector>
 
 
 using namespace std;
+
+float ranf();
+float box_muller(float m, float s);
+
+double GaussianDistribution(double x, double mu, double sigma){
+	double value = 0.0;
+
+	value = 1/(sigma*sqrt(2*M_PI)) * exp(-1.0/2.0 * pow( ((x-mu)/sigma),2));
+
+	return value;
+}
+
+
+int main() {
+
+	srand(time(0)); // set initial seed value to system clock
+
+	std::string strOutputFileName = "output.txt";
+	std::ofstream output(strOutputFileName.c_str());
+
+	int nDebts = 60000;    				//total number of debts
+	int nStatisticGroups = 100; 		//number of groups for statistical purposes
+
+
+	vector<double> vdProbabilities1;		//probabilities for total number of IOU's
+	vector<double> vdProbabilitiesCumulative1;
+	vector<double> vdProbabilities2;		//probabilities for average amount of each IOU per account
+	vector<double> vdProbabilitiesCumulative2;
+
+	double cumulativeProbability1 = 0.0;
+	double cumulativeProbability2 = 0.0;
+	for(int i = 0; i < nStatisticGroups; i++){
+		double probability1 = GaussianDistribution((double) i/nStatisticGroups, 0.2, 0.3);
+		vdProbabilities1.push_back(probability1);
+		double probability2 = GaussianDistribution((double) i/nStatisticGroups, 0.5, 0.1);
+		vdProbabilities2.push_back(probability2);
+
+		vdProbabilitiesCumulative1.push_back(cumulativeProbability1);
+		cumulativeProbability1 += probability1;
+		vdProbabilitiesCumulative2.push_back(cumulativeProbability2);
+		cumulativeProbability2 += probability2;
+	}
+
+	int nLow = 0;
+	int nHigh = RAND_MAX;
+
+	double dRandom1;
+	double dRandom2;
+
+	for(int i = 0; i < nDebts; i++){
+
+		stringstream ss;
+
+		dRandom1 = ((double)((rand() % (nHigh - nLow + 1)) + nLow)/nHigh) * cumulativeProbability1 ;
+		for(unsigned int i = 0; i < vdProbabilitiesCumulative1.size()-1; i++){
+			if(vdProbabilitiesCumulative1.at(i) <= dRandom1 && vdProbabilitiesCumulative1.at(i+1) >= dRandom1 ){
+				ss << i+1 << "_" ;
+				break;
+			}
+		}
+
+		dRandom2 = ((double)((rand() % (nHigh - nLow + 1)) + nLow)/nHigh) * cumulativeProbability2 ;
+		for(unsigned int i = 0; i < vdProbabilitiesCumulative2.size()-1; i++){
+			if(vdProbabilitiesCumulative2.at(i) <= dRandom2 && vdProbabilitiesCumulative2.at(i+1) >= dRandom2 ){
+				ss << i+1 ;
+				break;
+			}
+		}
+
+		output << ss.str() << endl;
+
+	}
+
+	return 0;
+}
+
+
 
 /***************************************************************************************************************
 boxmuller.c Implements the Polar form of the Box-Muller
@@ -63,15 +143,3 @@ use_last = 1;
 return( m + y1 * s );
 }
 
-
-
-
-
-int main() {
-	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
-	for(int i = 0; i < 10; i++){
-		cout << box_muller(5.0, 1.0) << endl;
-	}
-
-	return 0;
-}
